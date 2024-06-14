2f58a0145f97ef5e429d601ff8ca2dcf3aec4c17
jong-un <36259838+zwong91@users.noreply.github.com>
2024-06-14 13:43:30 +0800

    chore: Update dependencies and fix import paths in frontend code

--- frontend/src/wallets/near.js
+++ frontend/src/wallets/near.js
@@ -1,13 +1,13 @@
 // near api js
-import { providers } from 'near-api-js';
+import { providers } from 'unc-api-js';
 
 // wallet selector
 import { distinctUntilChanged, map } from 'rxjs';
-import '@near-wallet-selector/modal-ui/styles.css';
-import { setupModal } from '@near-wallet-selector/modal-ui';
-import { setupWalletSelector } from '@near-wallet-selector/core';
-import { setupHereWallet } from '@near-wallet-selector/here-wallet';
-import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
+import '@unc-wallet-selector/modal-ui/styles.css';
+import { setupModal } from '@unc-wallet-selector/modal-ui';
+import { setupWalletSelector } from '@unc-wallet-selector/core';
+import { setupHereWallet } from '@unc-wallet-selector/here-wallet';
+import { setupMyNearWallet } from '@unc-wallet-selector/my-near-wallet';
 
 const THIRTY_TGAS = '30000000000000';
 const NO_DEPOSIT = '0';
