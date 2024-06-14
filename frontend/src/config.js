eeb284a255cdbf18003d028f4fe5d977b8f5846e
jong-un <36259838+zwong91@users.noreply.github.com>
2024-06-14 14:52:08 +0800

    chore: Remove unused GitHub Actions workflow for tests-ts.yml and update .gitignore

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 const contractPerNetwork = {
-  mainnet: 'hello.near-examples.near',
-  testnet: 'hello.near-examples.testnet',
+  mainnet: '377ac29dcdcf80f8741c987df93e6381bc4dd36686eff06780438d1fc10970ef',
+  testnet: '377ac29dcdcf80f8741c987df93e6381bc4dd36686eff06780438d1fc10970ef',
 };
 
 export const NetworkId = 'testnet';
